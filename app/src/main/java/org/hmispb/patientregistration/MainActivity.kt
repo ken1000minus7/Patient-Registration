311cd1c9a6ad7bee7c45f08a7d58f6ee5af456c9
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-09-03 12:36:51 +0530

    added date picker

--- app/src/main/java/org/hmispb/patientregistration/MainActivity.kt
+++ app/src/main/java/org/hmispb/patientregistration/MainActivity.kt
@@ -1,11 +1,28 @@
 package org.hmispb.patientregistration
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import org.hmispb.patientregistration.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val calendar = Calendar.getInstance()
+        setContentView(binding.root)
+        binding.DOBTextBox.setOnClickListener {
+            val dialog = DatePickerDialog(this@MainActivity, { view, year, month, day ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH,month)
+                calendar.set(Calendar.DAY_OF_MONTH,day)
+                binding.DOBTextBox.text = "${calendar.get(Calendar.DAY_OF_MONTH)}/${calendar.get(Calendar.MONTH)}/${calendar.get(Calendar.YEAR)}"
+            },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH))
+            dialog.show()
+        }
     }
 }
