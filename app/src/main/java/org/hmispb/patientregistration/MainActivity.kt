479dee5415b44cb1cf1fe6cd20167c8168b0db61
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-09-07 18:22:05 +0530

    Revert "Revert "working on stuff""
    This reverts commit 1553681a8a609916decd132aa9d4b6be77200870.

--- app/src/main/java/org/hmispb/patientregistration/MainActivity.kt
+++ app/src/main/java/org/hmispb/patientregistration/MainActivity.kt
@@ -1,9 +1,15 @@
 package org.hmispb.patientregistration
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import org.hmispb.patientregistration.Utils.OPD_COUNTER
+import org.hmispb.patientregistration.Utils.OPD_ID
+import org.hmispb.patientregistration.Utils.PREV_DATE
 import org.hmispb.patientregistration.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
@@ -11,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val sharedPreferences = getSharedPreferences(OPD_COUNTER, MODE_PRIVATE)
+        val dateString = sharedPreferences.getString(PREV_DATE,"")
+        val prevDate = if(dateString.isNullOrEmpty()) Date(1) else Gson().fromJson(dateString,Date::class.java)
+        val currentDate = Date()
+        if(prevDate.before(currentDate) && prevDate.day!=currentDate.day) {
+            sharedPreferences.edit()
+                .putInt(OPD_ID,0)
+                .putString(PREV_DATE,Gson().toJson(currentDate))
+                .commit()
+        }
+
         binding.btnNewUser.setOnClickListener {
         val intent = Intent(this, NewPatientActivity::class.java)
             startActivity(intent)
