ee5ce9807938904c20611bc5747422dca1b78785
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-09-11 23:01:07 +0530

    nab mistake now over pls :sob:

--- app/src/main/java/org/hmispb/patientregistration/room/PatientDatabase.kt
+++ app/src/main/java/org/hmispb/patientregistration/room/PatientDatabase.kt
@@ -2,7 +2,6 @@ package org.hmispb.patientregistration.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import org.hmispb.patientregistration.model.Patient
 
