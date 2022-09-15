dc7f7eefad2f0dc6c0746a5986fc055b03574060
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-09-15 23:05:59 +0530

    gov nab not my fault :whistle:

--- app/src/main/java/org/hmispb/patientregistration/model/SavePatientRequest.kt
+++ app/src/main/java/org/hmispb/patientregistration/model/SavePatientRequest.kt
@@ -1,9 +1,8 @@
 package org.hmispb.patientregistration.model
 
 data class SavePatientRequest(
-    //TODO take hospital code and seat ID from Login api
-    val hospitalCode : String = "998",//for now
-    val seatId : String = "10001",
+    val hospitalCode : String,
+    val seatId : String,
     val inputDataJson : String,
-    val modeForData : String = "REGISTER_NEW_PATIENT"
+    val modeFordata : String = "REGISTER_NEW_PATIENT"
 )
